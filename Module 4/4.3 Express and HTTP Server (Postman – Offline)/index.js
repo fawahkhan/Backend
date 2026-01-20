18be2e5a5d940528858be551d0d5cbf1aa8b1ecd
fawahkhan <khanmohdfawah21@gmail.com>
2026-01-20 23:26:02 +0530

    made a code to check the kidneys and its logic

--- Module 4/4.3 Express and HTTP Server (Postman – Offline)/index.js
+++ Module 4/4.3 Express and HTTP Server (Postman – Offline)/index.js
@@ -2,6 +2,20 @@ const express = require ('express')
 
 const app = express();  //yeh hr bari likhna hi likhna hai
 
+// in memory hospital with 4 routes get post put and delete . 
+
+var users = [{
+    name: 'John' ,
+    kidneys: [{
+        healthy : false
+    },{
+        healthy : true
+    }
+    ]
+}]
+console.log(users[0])
+
+app.use(express.json())
 
 
 // function sum(n){
@@ -11,9 +25,59 @@ const app = express();  //yeh hr bari likhna hi likhna hai
 //     }
 //     return ans
 // }
-
+// a popular input type is query parameter in get requests.
 app.get('/',  function(req , res){
-    res.send("hi there")
+    // here we have to get the no of kidneys a user has and their health .
+    const johnKidneys = users[0].kidneys
+    const numberOfKidneys = johnKidneys.length
+    let numberOfHealthyKidneys = 0 ;
+    for (let i = 0 ; i<johnKidneys.length ; i++){
+        if(johnKidneys[i].healthy){
+            numberOfHealthyKidneys = numberOfHealthyKidneys + 1 ;
+        }
+    }
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
+})
+
+// in post requests a popular input type is sending data in the body
+
+app.post('/',  function(req , res){
+    //here we want to add a new kidney
+    const isHealthy = req.body.isHealthy
+    users[0].kidneys.push({
+        healthy: isHealthy
+
+    })
+    res.json({
+        msg: 'done!'
+    })
+})
+app.put('/',  function(req , res){
+    // here we want to replace a kidney , make it healthy 
+    for(let i = 0 ; i< users[0].kidneys.length; i++){
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({})
+})
+app.delete('/',  function(req , res){
+    // here we want to delete a kidney
+    const newKidney = []
+    for (let i= 0 ; i< users[0].kidneys.length ; i++){
+        if(users[0].kidneys[i].healthy){
+            newKidney.push({
+                healthy : true
+            })
+        }
+    }
+    users[0].kidneys = newKidney 
+    res.json({
+        msg: "done"
+    })
 })
 
 app.listen(3000);
