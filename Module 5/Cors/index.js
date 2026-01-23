b91343b451d26916fdba193014b786e3f71a92a8
fawahkhan <khanmohdfawah21@gmail.com>
2026-01-23 22:19:17 +0530

    Resolved the cors error when trying to share resources between different origins by using cors

--- Module 5/Cors/index.js
+++ Module 5/Cors/index.js
@@ -2,9 +2,16 @@
 //the frontend will be running on a different port thus without cors it wont be able to access the backend and serve the request
 
 const express = require ("express")
+const cors = require ("cors")
 
 const app=express()
-
+app.use(cors())   //this will allow all frontends to access our backend
+ // to allow only required frontend to access our data do this
+ /*
+ app.use(cors({
+    domains : ["https://google.com", "https://facebook.com"]      //this will allow only google and fb to access our backend
+ }))
+ */ 
 app.use(express.json())   //middleware--> if we dont use this then req.body will be undefined
 
 app.post("/sum", function(req,res){
@@ -12,7 +19,7 @@ app.post("/sum", function(req,res){
     const b = parseInt(req.body.b)
 
     res.json({
-        result : a+b
+        answer : a+b
     })
 })
 
