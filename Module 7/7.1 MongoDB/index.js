4be412796d65ef36b9fc99b67ebb4e7ec5c6faaf
fawahkhan <khanmohdfawah21@gmail.com>
2026-01-29 22:54:19 +0530

    Mongoose setup and endpoints created

---
+++ Module 7/7.1 MongoDB/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.post('/signup', function(req, res){
+
+});
+
+app.post('/signin', function(req, res){
+
+});
+
+//to create a todo
+app.post('/todo', function(req, res){
+
+});
+
+app.get('/todos', function(req, res){
+
+});
+
+app.get(3000)
